729f360185702aaeb523fb44f5f5ed8260935cf4
GabrielBiasoli <bielbiasoli33@icloud.com>
2025-07-06 18:46:04 -0300

    Creating asset folder Inserting all assets Creating the menu: background, sound and fonts Creating const file

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+# C
+COLOR_GREEN = (72, 150, 44)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 1536
+WIN_HEIGHT= 1000
